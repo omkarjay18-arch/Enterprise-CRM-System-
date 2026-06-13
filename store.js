be18f9b9aa570d3d302102342e27cc6abbd61c96
omkarjay18-arch <omkarjay18@gmail.com>
2026-06-13 16:50:43 +0530

    Add files via upload

---
+++ store.js
@@ -0,0 +1,26 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query/react'
+import { snazApi } from '@/features/api/auth'
+
+import tasksReducer from '@/features/tasks/slice'
+import authReducer from '@/features/auth/slice'
+import dealsReducer from '@/features/deals/slice'
+import companiesReducer from "@/features/companies/companies-slice";
+
+export const store = configureStore({
+  reducer: {
+    [snazApi.reducerPath]: snazApi.reducer,
+    tasks: tasksReducer,
+    auth: authReducer,
+    deals: dealsReducer,
+    companies: companiesReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      immutableCheck: false,
+    }).concat(snazApi.middleware),
+})
+
+// required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch);
